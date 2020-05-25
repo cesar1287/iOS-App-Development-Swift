e79be5a8e1e93ab8b941e2d0bc065974634f346d
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-05-24 22:14:17 -0300

    week2

--- course2/ViewController.swift
+++ course2/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isAndroidImage = true
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var buttonOtherImage: UIButton!
+    
+    @IBAction func onButtonOtherImagePressed(_ sender: UIButton) {
+        if (isAndroidImage) {
+            imageView.image = UIImage(named: "ios")
+            isAndroidImage = false
+        } else {
+            imageView.image = UIImage(named: "android")
+            isAndroidImage = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(named: "android")
     }
 
 
